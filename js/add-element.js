49d2d3b14349daef3edc0f5dcea08a11c9f8cf6c
srostron2112 <srostron@usca.edu>
2021-11-10 12:25:43 -0500

    2 files modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,25 +3,34 @@
  */
 
 // get all the hot class elements- change them to cool class
-$(".hot").each(function(){
-  $(this).removeClass("hot");
-  $(this).addClass("cool");
+$('li').each(function(){
+  $(this).removeClass('hot');
+  $(this).addClass('cool');
 });
-
 // traverse the elements
-$('#one').next().next().text("milk");
+$('#one').next().next().text('milk');
+$('#one').remove();
 // add a new element by clicking the plus sign
-$('#todo').append('<li>Candy<li>');
-$('#add').click(addElement);
+$('#todo').append("<li>candy</li>");
 // before and after are for siblings
 // append and prepend are for parent
-
+$('#add').click(addElement);
 function addElement() {
   // add a new element
-  $('#todo').append('<li> <li>');
   // add a input text box
+  $('#todo').append('<li><input type="text"></li>');
   // whenever the user are done add the element
-  
+  $('input').blur(function(){
+    $(this).parent().addClass('cool');
+    var items = $(this).val();
+    //replace the input box by the text
+    $(this).parent().text(items);
+
+    //click the new list will change the style
+    $('input').each(function(){
+      $(this).addClass('cool');
+    })
+  });
 }
 
 // bind click with the event handler
@@ -33,15 +42,16 @@ function changeStyle() {
     $(this).removeClass('cool');
     $(this).addClass('complete');
   }
-  else if($(this).hasClass('')) {
-    $(this).removeClass('');
-    $(this).addClass('complete');
-  }
-  else{
+  else if($(this).hasClass('complete')){
     $(this).removeClass('complete');
     $(this).addClass('cool');
   }
-
+  else if($(this).hasClass('hot')){
+    $(this).removeClass('hot');
+    $(this).addClass('cool');
+  }else{
+    $(this).addClass('complete');
+  }
 }
 
 // delete complete element by clicking the trash can
@@ -49,4 +59,6 @@ document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
+  
 }
